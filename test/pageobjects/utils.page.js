cdaf8d8e7dc121283d387f7c60a9daec96789512
Joshua Cadavez <jcadavez@insider.com>
2020-09-29 13:23:00 -0400

    Completed broken link automation functionality

--- test/pageobjects/utils.page.js
+++ test/pageobjects/utils.page.js
@@ -3,9 +3,15 @@ module.exports = {
         let ind = url.lastIndexOf("https://");
         url = url.substring(ind+"https://".length);
         ind = url.indexOf("&");
+        // console.log("substring...");
+        // console.log(url);
         if(ind > 0) url = url.substring(0,ind);
+        // console.log("&...");
+        // console.log(url);
         ind = url.indexOf("?");
         if(ind > 0) url = url.substring(0,ind);
-        return url.substring(0,ind);
+        // console.log("?...");
+        // console.log(url);
+        return url.trim();
     }
 };
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,37 +1,35 @@
-const fetch = require('node-fetch');
-var linkScraper = require("link-scraper");
 const utils = require('../pageobjects/utils.page.js');
 
 describe('BI webpage', () => {
     it('should get all links', function() {
         browser.url("/best-interactive-cat-toys");
         let urls = $$('.insider-pick-button.headline-bold').map(link => decodeURIComponent(link.getAttribute('href')));
-        let affiliate = urls.map(url => {
-            return utils.trimUrl(url);
-        });
+        let affiliate = urls.map(url => utils.trimUrl(url));
         urls = new Set(urls);
         affiliate = new Set(affiliate);
         let statusMap = new Map();
         browser.cdp('Network', 'enable');
         browser.on('Network.responseReceived', params => {
             const url = utils.trimUrl(params.response.url);
-            if(affiliate.has(url)){
+            if(url !== "" && affiliate.has(url) && !statusMap.has(url)){
+                console.log("Url : "+url+"\tResponse : "+params.response.status);
                 statusMap.set(url, params.response.status);
             }
         });
         urls.forEach(url => {
             console.log("===================================");
-            console.log(`Navigating to ${url}...`);
-            browser.url(url);
+            statusMap = new Map();
             try {
+                console.log(`Navigating to ${url}`);
+                browser.url(url);
                 const affiliateUrl = utils.trimUrl(url);
                 browser.waitUntil(() => statusMap.has(affiliateUrl), {
-                    timeout: 3000,
-                    timeoutMsg: `After 3s, cannot find network response for: ${url}`
+                    timeout: 2000,
+                    timeoutMsg: `After 2.0s, cannot find network response for: ${affiliateUrl}`
                 });
                 console.log(`Status is : ${statusMap.get(affiliateUrl)}`);
             } catch(error){
-                console.log(`Error : ${error}`);
+                console.log(`${error.name}\n${error.message}`);
             } finally{
                 browser.back();
             }
